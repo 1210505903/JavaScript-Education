285eef3dd1bbca711493897f0a4d4ecc43642545
Graudss <yusuf.guner.766@gmail.com>
2023-08-01 15:10:49 +0300

    update

--- b_eklenti/main.js
+++ b_eklenti/main.js
@@ -1,10 +1,51 @@
 
-function toggleeeee() {
-    const parg = document.getElementById("info");
-    if (parg.style.display === "none") {
-        parg.style.display = "block";
+function toggleDiv(divNum) {
+    
+    var div = document.getElementById("div" + divNum);
+    var allDivs = document.querySelectorAll(".hidden-div");
+
+    for (var i = 0; i < allDivs.length; i++) {
+        if (allDivs[i] !== div) {
+            allDivs[i].style.display = "none";
+        }
+    }
+
+    if (div.style.display === "none") {
+        div.style.display = "block";
     } else {
-        parg.style.display = "none";
+        div.style.display = "none";
+    }
+}
+function toggleButtons() {
+    const button2 = document.getElementById("btn2");
+    const button3 = document.getElementById("btn3");
+    const button4 = document.getElementById("btn4");
+
+    if (button3.classList.contains("hidden") && button4.classList.contains("hidden")) {
+        button3.classList.remove("hidden");
+        button4.classList.remove("hidden");
+        button2.classList.add("hidden");
+        button3.style.marginTop = "-500px";
+    } else {
+        button3.classList.add("hidden");
+        button4.classList.add("hidden");
+        button2.classList.remove("hidden");
+    }
+}
+function toggleButtonss() {
+    const button1 = document.getElementById("btn1");
+    const button5 = document.getElementById("btn5");
+    const button6 = document.getElementById("btn6");
+
+    if (button5.classList.contains("hidden") && button6.classList.contains("hidden")) {
+        button5.classList.remove("hidden");
+        button6.classList.remove("hidden");
+        button1.classList.add("hidden");
+
+    } else {
+        button5.classList.add("hidden");
+        button6.classList.add("hidden");
+        button1.classList.remove("hidden");
     }
 }
 function toggleModal() {
@@ -18,7 +59,7 @@ function toggleModal() {
     } else {
         closeModal();
     }
- }
+}
 
 function closeModal() {
     const bubble = document.getElementById("bubble")
